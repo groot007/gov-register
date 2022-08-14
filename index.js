944980d775c4ef4bff7f47b6c05180c9c461c19b
Mykola Stanislavchuk <m.stanislavchuk@astoundcommerce.com>
2022-08-14 21:15:07 +0200

    port env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
